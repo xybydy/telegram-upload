5fd1be88efb0250dbfcbd1af46fbc1c2060e89a5
Fatih Ka <xybydy@gmail.com>
2022-12-19 19:25:22 +0300

    f

--- telegram_upload/client.py
+++ telegram_upload/client.py
@@ -175,7 +175,7 @@ class Client(TelegramClient):
             has_files = True
             if self.file_exist(entity,file.file_name):
                 click.echo('File already exists "{}")'.format(file.file_name))
-                with open("eh.txt","a") as fii:
+                with open("eh.txt","a") as fi:
                     fi.write(file.file_name+"\n")
                 continue
 
